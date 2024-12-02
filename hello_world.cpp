e069ccb091eb67f4cb615287a67344992017abd6
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-12-02 21:51:39 +0300

    style: reformatted code using clang-format with Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
- # include <iostream>
+#include <iostream>
 using namespace std;
-int main() {
-    string name;
-    cout << "Enter your name, please: ";
-    cin >> name;
-    cout << "Hello world from " << name;
-    return 0;
+int
+main()
+{
+  string name;
+  cout << "Enter your name, please: ";
+  cin >> name;
+  cout << "Hello world from " << name;
+  return 0;
 }
