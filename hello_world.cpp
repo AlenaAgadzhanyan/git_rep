c8c0928c3e5b0d15eac52b211867e4dafbe742b5
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-12-02 20:55:36 +0300

    docs: add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
  # include <iostream>
 int main() {
+    // переменная для имени пользователя
     std::string name;
     std::cout << "Enter your name, please: ";
+    // ввод имени пользователя
     std::cin >> name;
+    // вывод приветствия
     std::cout << "Hello world from " << name;
     return 0;
 }
